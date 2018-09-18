888396d5460a34c77f63b2fbe00d57e719bc9357
fkatleader2 <fkatleader3@gmail.com>
2018-09-17 23:11:44 -0400

    socket url change

--- game/modules/tetriasocket.js
+++ game/modules/tetriasocket.js
@@ -2,7 +2,7 @@ tetresse.modules.tetriasocket = {
     socket: null,
     setup() {
         try {
-            this.socket = io();            
+            this.socket = io('https://tetria.tetresse.com:8000');
         } catch(e) {
             console.log("offline mode");
         }
